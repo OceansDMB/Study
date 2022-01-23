9e1ffa8eb5a18f9ebb1d7b231cc90df2fbf0f6a4
Dylan <qkrekgka2003@gmail.com>
2022-01-23 23:48:00 +0900

    객체지향의 다형성(부모 클래스에서의 메서드중 자식 클래스에서 해당 메서드를 다른동작하게, 클래스 생성자 함수 constructor() , hasOwnProperty 사용법

--- templete_source/index.js
+++ templete_source/index.js
@@ -183,7 +183,7 @@ console.log(user.age, user.name, user.get_data());
 
   console.log(user.age,user.name,user.get_data()); */
 
-  function User(age,name) {
+/*   function User(age,name) {
     this.age = age;
     this.name = name;
     this.get_data = function(){
@@ -193,4 +193,113 @@ console.log(user.age, user.name, user.get_data());
 
   const dave = new User(10,'dave');
   console.log(typeof dave);
-  console.log(dave.age,dave.get_data());
+  console.log(dave.age,dave.get_data()); */
+
+
+  class User1 {
+    constructor(name, age) {
+      this.name = name;
+      this.age = age;
+    }
+
+    get_message() {
+      return 'Hello!';
+    }
+  }
+
+  const dave = new User1('dave',30);
+  console.log(typeof dave, dave.name, dave.age, dave.get_message()); 
+
+  const dylan = new User1('dylan',31);
+  console.log(typeof dylan, dylan.name, dylan.age, dylan.get_message()); 
+
+  /* 
+    1.class 내부에 constructor()라는 이름으로 하나의 생성자 함수는 작성할 수있음
+    function 이라는 키워드 사용하지 않음. 
+    2.class 로 정의된 클래스는 new 클래스명() 으로 객체 생성할 수 있음.
+    3.class property는 constructor 내부에서 this 키워드로 선언될 수도 있음
+    4.객체 생성시 인자 정의는 constructor에서 할 수 있음
+    5.class 내부에 메서드 선언 가능
+
+    상속도 일반적인 객체 지향 문법과 유사함
+
+    1. extends를 사용해서, 상속할 클래스를 선언 할 수 있음.
+    2. 자식 클래스에서는 super()(부모 클래스의 constructor()를 호출함)
+        를 constructor() 안에서 호출 해야 함.
+    
+    */
+
+/*   class Animal {
+    constructor(name){
+      this.name = name;
+    }
+  }
+
+  class User extends Animal {
+    constructor(name, brand) {
+      super(name);
+      this.brand = brand;
+    }
+  }
+
+  const dave = new User('Dylan' , 'Funcoding');
+  console.log(dave.name, dave.brand); */
+
+  /* 객체지향의 다형성 (부모 클래스의 메서드명과 동일하지만, 자식 클래스에서
+     해당 메서드를 덮어 씌워 다른 동작을 할 수 있는 기능)도 지원됨 */
+  
+/*   class Animal {
+    constructor (name) {
+      this.name = name;
+    }
+  
+    get_message(){
+      return 'Hello';
+    }
+  }
+
+  class User extends Animal {
+    constructor (name,age) {
+      super(name);
+      this.age = age;
+    }
+
+    get_message(){
+      return 'Bye'
+    }
+  }
+
+  const dylan = new  User(dylan,31);
+  console.log(dylan.name,dylan.age,dylan.get_message()); */
+
+  /* hasOwnProperty() 사용법 
+    1. 클래스명.prototype.프로퍼티 = 프로퍼티 값 으로도 클래스 외부에서 프로퍼티
+    추가 가능(클래스 내부에서 선언하는 것을 추천 하지만, 이 케이스를 
+      문법 에러로 막지는 않았음)
+
+    2. 클래스 내부에서 선언한 프로퍼티임을 확인하기 위해 hasOwnProperty(프로퍼티명)
+      을 사용 할 수 있음. 
+
+    3. 정의한 property가 외부에서 정의한것인지 내부에서 정의한 것인지를 확인.
+    
+    4. property 만 사용 가능하고 method 는 사용 불가능.
+  */
+
+  class Animal {
+    constructor(name){
+      this.name = name;
+    }
+    get_message() {
+      return 'Hello';
+    }
+  }
+
+  Animal.prototype.age = 10;
+
+  const dave = new Animal('Dave');
+  console.log(dave.hasOwnProperty('name'));
+  console.log(dave.hasOwnProperty('age'));
+
+
+
+  console.log(dave);
