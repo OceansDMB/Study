1bdcc725bea1580fdf540563a4ed182b8f9c841f
Dylan <qkrekgka2003@gmail.com>
2022-02-11 20:11:17 +0900

    디자인 관련 사이트 크롤링, 개인 블로그에 추가

--- templete_source/js/main.js
+++ templete_source/js/main.js
@@ -1,6 +1,6 @@
 const backToTop = document.getElementById("backtotop");
 
-function checkScroll() {
+const checkScroll = () => {
   /* 
         웹 페이지가 수직으로 얼마나 스크롤되었는지를 확인하는 값
         (픽셀 단위로 변환)
@@ -14,9 +14,9 @@ function checkScroll() {
   } else {
     backToTop.classList.remove("show");
   }
-}
+};
 
-function moveBackToTop() {
+const moveBackToTop = () => {
   if (window.pageYOffset > 0) {
     /* 
         smooth 하기 scroll 하기 
@@ -24,7 +24,39 @@ function moveBackToTop() {
       */
     window.scrollTo({ top: 0, behavior: "smooth" });
   }
-}
+};
 
 window.addEventListener("scroll", checkScroll);
 backToTop.addEventListener("click", moveBackToTop);
+
+/* --------------------------------------------------------------------- */
+
+// 서로가 서로를 호출하는 함수를 선언하게 될 경우
+/* const transformPrev = () => {
+
+
+}; */
+
+const slidePrevList = document.getElementsByClassName("slide-prev");
+
+for (let i = 0; i < slidePrevList.length; i++) {
+  //ul tag 선택
+  let classList =
+    slidePrevList[i].parentElement.parentElement.nextElementSibling;
+  let liList = classList.getElementsByTagName("li");
+
+  // 카드가 ul 태그 너비보다 넘치면, 왼쪽(PREV) 버튼을 활성화 하고, 오른쪽(NEXT)
+  // 는 현재 첫 카드 위치이므로 비활성화.
+  if (classList.clientWidth < liList.length * 260) {
+    slidePrevList[i].classList.add("slide-prev-hover");
+    slidePrevList[i].addEventListener("click", transformPrev);
+  } else {
+    /* 태그 삭제시, 부모 요소에서 removeChild 를 통해 삭제해야 함 
+      따라서, 1. 먼저 부모 요소를 찾아서, 
+              2. 부모 요소의 자식 요소로 있는 PREV와 NEXT 요소를 삭제함
+    */
+    const arrowContainer = slidePrevList[i].parentElement;
+    arrowContainer.removeChild(slidePrevList[i].nextElementSibling);
+    arrowContainer.removeChild(slidePrevList[i]);
+  }
+}
