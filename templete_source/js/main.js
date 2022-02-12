9e5ac393871980b672de0049ff76ac09e1de4fb5
Dylan <qkrekgka2003@gmail.com>
2022-02-12 23:33:15 +0900

    개인 Blog 및 동적화개선

--- templete_source/js/main.js
+++ templete_source/js/main.js
@@ -2,10 +2,9 @@ const backToTop = document.getElementById("backtotop");
 
 const checkScroll = () => {
   /* 
-        웹 페이지가 수직으로 얼마나 스크롤되었는지를 확인하는 값
-        (픽셀 단위로 변환)
-        https://developer.mozilla.org/ko/docs/Web/API/window/pageYOffset
-    */
+  웹 페이지가 수직으로 얼마나 스크롤되었는지를 확인하는 값 (픽셀 단위로 변환)
+  https://developer.mozilla.org/ko/docs/Web/API/window/pageYOffset
+  */
 
   let pageYOffset = window.pageYOffset;
 
@@ -19,8 +18,8 @@ const checkScroll = () => {
 const moveBackToTop = () => {
   if (window.pageYOffset > 0) {
     /* 
-        smooth 하기 scroll 하기 
-        https://developer.mozilla.org/en-US/docs/Web/API/Element/scrollTo
+  smooth 하기 scroll 하기 
+  https://developer.mozilla.org/en-US/docs/Web/API/Element/scrollTo
       */
     window.scrollTo({ top: 0, behavior: "smooth" });
   }
@@ -32,10 +31,39 @@ backToTop.addEventListener("click", moveBackToTop);
 /* --------------------------------------------------------------------- */
 
 // 서로가 서로를 호출하는 함수를 선언하게 될 경우
-/* const transformPrev = () => {
+/* const transformPrev = () => {}; 처럼 선언하게 될 경우 
+const 로 선언하면 호이스팅 이슈로 선언이 되지않아 에러가 발생함.
+*/
+function transformPrev(event) {
+  const slidePrev = event.target;
+  const slideNext = slidePrev.nextElementSibling;
 
+  // 관련된 ul 태그 선택
+  const classList = slidePrev.parentElement.parentElement.nextElementSibling;
+  let activeLi = classList.getAttribute("data-position");
+  const liList = classList.getElementsByTagName("li");
 
-}; */
+  /* classList.clientWidth 는 ul tag의 실질적인 너비
+   * liList.length * 260 에서 260은 각 li 요소의 실질 너비(margin 포함)
+   * activeLi 는 data-position 에 있는 현재 위치
+   * liList.length * 260 + Number(activeLi)는 현재 위치부터 오른쪽으로
+   * 나열되야 하는 나머지 카드들의 너비
+   */
+
+  /* classList.clientWidth < (liList.length * 260 + Number(activeLi))
+   * 의미는 오른쪽으로 나열될 카드들이 넘친 상태이므로, 왼쪽으로 이동이 가능함
+   */
+
+  if (classList.clientWidth < liList.length * 260 + Number(activeLi)) {
+    activeLi = Number(activeLi) - 260;
+
+    slideNext.style.color = "#2f3059";
+    slideNext.classList.add("slide-next-hover");
+  }
+  classList.style.transition = "transform 1s";
+  classList.style.transform = "translateX(" + String(activeLi) + "px)";
+  classList.setAttribute("data-position", activeLi);
+}
 
 const slidePrevList = document.getElementsByClassName("slide-prev");
 
@@ -51,10 +79,10 @@ for (let i = 0; i < slidePrevList.length; i++) {
     slidePrevList[i].classList.add("slide-prev-hover");
     slidePrevList[i].addEventListener("click", transformPrev);
   } else {
-    /* 태그 삭제시, 부모 요소에서 removeChild 를 통해 삭제해야 함 
-      따라서, 1. 먼저 부모 요소를 찾아서, 
-              2. 부모 요소의 자식 요소로 있는 PREV와 NEXT 요소를 삭제함
-    */
+    /* 태그 삭제시, 부모 요소에서 removeChild 를 통해 삭제해야 함 따라서 
+  1. 먼저 부모 요소를 찾아서, 
+  2. 부모 요소의 자식 요소로 있는 PREV와 NEXT 요소를 삭제함
+  */
     const arrowContainer = slidePrevList[i].parentElement;
     arrowContainer.removeChild(slidePrevList[i].nextElementSibling);
     arrowContainer.removeChild(slidePrevList[i]);
