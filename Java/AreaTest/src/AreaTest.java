aea8aa2bc3d4f532bdcdc75bcee29e217b71bf58
OceansProgram <qkrekgka2003@gmail.com>
2022-03-14 21:28:19 +0900

    TIL3.14

---
+++ Java/AreaTest/src/AreaTest.java
@@ -0,0 +1,11 @@
+
+public class AreaTest {
+	public static void main(String args[]) {
+		double radius,area;
+		
+		radius = 5.0;
+		area = 3.141592 * radius * radius;
+		
+		System.out.println("원의 면적은 " + area + "입니다.");
+	}
+}
--- Java/Hello2/src/Hello2.java
+++ Java/Hello2/src/Hello2.java
@@ -7,4 +7,5 @@ public class Hello2 {
 			System.out.println("자바를 처음 공부합니다.");
 			System.out.println("자바는 재미있나요?");
 		}
+		
 }
