5a034dc0c6f68db5b7d799c2f1aa1de7bca336e0
Oceansprogram <qkrekgka2003@gmail.com>
2022-03-16 14:56:36 +0900

    TIL 3.16 React project

---
+++ React/blog/src/App.js
@@ -0,0 +1,17 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  let posts = "강남 고기 맛집";
+  return (
+    <div className="App">
+      <div className="black-nav">
+        <div className={posts}>개발 Blog</div>
+      </div>
+      <img src={logo} />
+      <h4>{posts}</h4>
+    </div>
+  );
+}
+
+export default App;
