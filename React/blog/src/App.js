38bfd2371777004d8564109d542b8cac7a880449
OceansProgram <qkrekgka2003@gmail.com>
2022-03-21 00:10:44 +0900

    React - node.js error , reinstall

--- React/blog/src/App.js
+++ React/blog/src/App.js
@@ -1,34 +1,54 @@
-  import React,{useState} from "react";
-  import "./App.css"
+import React, { useState } from "react";
+import "./App.css";
 
+function App() {
+  let [title, titlech] = useState(["Front-End", "Back-End", "Servers"]);
+  let [thumbsup, thumbsupch] = useState([0, 0, 0]);
+  let [modal, modalch] = useState(false);
+  let [pushti, pushtich] = useState(0);
 
-  function App() {
-    let [title,titlech] = useState(["Front-End","Back-End","Servers"]);
-    let [thumbsup,thumbsupch] = useState([0,0,0]);
-    let [modal,modalch] = useState(false);
-    let [pushti,pushtich] = useState(0);
-
-    return (
-      <div className="top-bar">
-        <div className="black-nav">
-          <div>Programming Blog</div>
-        </div>
+  return (
+    <div className="top-bar">
+      <div className="black-nav">
+        <div>Programming Blog</div>
       </div>
-      {
-        title.map(function(글, i){
-          return (<div className="list">
-          <h3 onClick={() =>{pushtich(i)}}>
-          {글}
-          </h3>
-          <span onClick={()=>
-          {
-            thumbsupch(thumbsup+1);
-          }
-        }
-          </div>)
-        })
-      }
-    )
-  }
+      {title.map(function (글, i) {
+        return (
+          <div className="list">
+            <h3
+              onClick={() => {
+                pushtich(i);
+              }}
+            >
+              {글}
+            </h3>
+            <span
+              onClick={() => {
+                thumbsupch(thumbsup + 1);
+              }}
+            >
+              ❤
+            </span>
+            {thumbsup}
+            <p>3dnjf 16일 발행</p>
+            <hr />
+          </div>
+        );
+      })}
+      {modal === true ? (
+        <Switch2 title={title} pushti={pushti}></Switch2>
+      ) : null}
+    </div>
+  );
+}
+function Switch2(props) {
+  return (
+    <div className="Modal">
+      <h2>{props.title[props.pushti]}</h2>
+      <p>날짜</p>
+      <span>세부내용</span>
+    </div>
+  );
+}
 
-  export default App;
+export default App;
