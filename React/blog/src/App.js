3fea6be01c5ad666ecbc5071880760b622833f25
OceansProgram <qkrekgka2003@gmail.com>
2022-03-22 19:04:38 +0900

    3.22 React.Shoppingmall(3)

--- React/blog/src/App.js
+++ React/blog/src/App.js
@@ -2,13 +2,14 @@ import React, { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import { render } from "@testing-library/react";
+import data from "./data.js";
 
 function App() {
   let [글제목, 글제목변경] = useState(["front-end", "back-end", "others"]);
   let [따봉, 따봉변경] = useState(0);
   let [modal, modal변경] = useState(false);
   let [누른제목, 누른제목변경] = useState(0);
-  let [입력값,입력값변경] = useState('');
+  let [입력값, 입력값변경] = useState("");
 
   return (
     <div className="App">
@@ -41,21 +42,27 @@ function App() {
       })}
 
       <div className="publish">
-        <input onChange={ (e) => {입력값변경(e.target.value)}}/>
-        <button onClick={()=>{
-          var arrayCopy = [...글제목];
-          arrayCopy.unshift(입력값);
-          글제목변경(arrayCopy);
-        }}>저장</button>
+        <input
+          onChange={(e) => {
+            입력값변경(e.target.value);
+          }}
+        />
+        <button
+          onClick={() => {
+            var arrayCopy = [...글제목];
+            arrayCopy.unshift(입력값);
+            글제목변경(arrayCopy);
+          }}
+        >
+          저장
+        </button>
       </div>
-      <Profile/>
+      <Profile />
       <button onClick={() => modal변경(!modal)}>상세내용</button>
 
-      {
-      modal === true 
-      ? (<Switch2 글제목={글제목} 누른제목={누른제목}></Switch2>) 
-      : null
-      }
+      {modal === true ? (
+        <Switch2 글제목={글제목} 누른제목={누른제목}></Switch2>
+      ) : null}
     </div>
   );
 }
@@ -69,59 +76,55 @@ function Switch2(props) {
   );
 }
 
-
 // 예전버젼 react에서의 컴포넌트 생성법
 //  class : 변수/함수 보관하는 덩어리
 //  extends : 오른쪽에 있는 놈의 성질을 물려받겠습니다.
 //  constructor : clas의 변수/초기값 저장할 때 쓴다.
 
 class Profile extends React.Component {
-  constructor(){
+  constructor() {
     super();
-    this.state = { name : 'Kim',age : 30};
-  }
-  
-  changeName(){
-    this.setState({ name: "Park" })
+    this.state = { name: "Kim", age: 30 };
   }
 
-  render(){
-    return(
+  changeName() {
+    this.setState({ name: "Park" });
+  }
+
+  render() {
+    return (
       <div>
-      <h3>프로필입니다.</h3>
-      <p>저는 {this.state.name} 입니다.</p>
-      <button onClick={ this.changeName.bind(this) }>버튼</button>
+        <h3>프로필입니다.</h3>
+        <p>저는 {this.state.name} 입니다.</p>
+        <button onClick={this.changeName.bind(this)}>버튼</button>
       </div>
-      )
-    }
-  };
-    
+    );
+  }
+}
+
 export default App;
-
-
-
-
-
-
 
 // 변수로 저장하지 않고 state로 선언해두면 변경시에 HTML이 자동으로 재렌더링됨.
 // 리액트에서 UI를 만들 때 state 데이터를 이용함.
 
-  /*   function switching() {
+/*   function switching() {
     var newArray = [...글제목];
     newArray.sort();
     글제목변경(newArray);
   } */
 
-{/*       <button onClick={switching}>button</button> */}
+{
+  /*       <button onClick={switching}>button</button> */
+}
 
-
-  {/*     <button onClick={ () =>{누른제목변경(0)}}>버튼1</button>
+{
+  /*     <button onClick={ () =>{누른제목변경(0)}}>버튼1</button>
     <button onClick={ () =>{누른제목변경(1)}}>버튼2</button>
     <button onClick={ () =>{누른제목변경(2)}}>버튼3</button> */
-  }
+}
 
-  {/*       {입력값}
+{
+  /*       {입력값}
 <input onChange={ (e) => {입력값변경(e.target.value)}}/>
  */
-  }
+}
--- Shop/shop/src/App.js
+++ Shop/shop/src/App.js
@@ -1,104 +1,117 @@
-import React, { useState } from 'react';
-import { Navbar,Container,Nav,NavDropdown,Carousel } from 'react-bootstrap';
-import './App.css';
-import Data from './data.js';
+import React, { useState } from "react";
+import { Navbar, Container, Nav, NavDropdown, Carousel } from "react-bootstrap";
+import "./App.css";
+import Data from "./data.js";
 
 function App() {
-
-  let[shoes, shoes변경] = useState(Data);
+  let [shoes, shoes변경] = useState(Data);
 
   return (
     <div className="App">
-<br />
-<Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-  <Container>
-  <Navbar.Brand href="#home">ShoeShop</Navbar.Brand>
-  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-  <Navbar.Collapse id="responsive-navbar-nav">
-    <Nav className="me-auto">
-      <Nav.Link href="#features">Features</Nav.Link>
-      <Nav.Link href="#pricing">Pricing</Nav.Link>
-      <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-        <NavDropdown.Divider />
-        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-      </NavDropdown>
-    </Nav>
-    <Nav>
-      <Nav.Link href="#deets">More deets</Nav.Link>
-      <Nav.Link eventKey={2} href="#memes">
-        Dank memes
-      </Nav.Link>
-    </Nav>
-  </Navbar.Collapse>
-  </Container>
-</Navbar>
-<Carousel className='background'>
-  <Carousel.Item>
-    <img
-      className="d-block w-100"
-      src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTPP5letp-8ItRXn7RCN12nN3bjh3RsTiN8TA&usqp=CAU'
-      alt="First slide"
-    />
-    <Carousel.Caption>
-      <h3>20% Season OFF</h3>
-      <p>Always sold at a discounted price.</p>
-    </Carousel.Caption>
-  </Carousel.Item>
-  <Carousel.Item>
-    <img
-      className="d-block w-100"
-      src="https://image.shutterstock.com/image-photo/software-developer-programming-code-abstract-600w-577183882.jpg"
-      alt="Second slide"
-      
-    />
+      <br />
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">ShoeShop</Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#features">Features</Nav.Link>
+              <Nav.Link href="#pricing">Pricing</Nav.Link>
+              <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">
+                  Another action
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">
+                  Something
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">
+                  Separated link
+                </NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+            <Nav>
+              <Nav.Link href="#deets">More deets</Nav.Link>
+              <Nav.Link eventKey={2} href="#memes">
+                Dank memes
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <Carousel className="background">
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTPP5letp-8ItRXn7RCN12nN3bjh3RsTiN8TA&usqp=CAU"
+            alt="First slide"
+          />
+          <Carousel.Caption>
+            <h3>20% Season OFF</h3>
+            <p>Always sold at a discounted price.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="https://image.shutterstock.com/image-photo/software-developer-programming-code-abstract-600w-577183882.jpg"
+            alt="Second slide"
+          />
 
-    <Carousel.Caption>
-      <h3>20% Season OFF</h3>
-      <p>Always sold at a discounted price.</p>
-    </Carousel.Caption>
-  </Carousel.Item>
-  <Carousel.Item>
-    <img
-      className="d-block w-100"
-      src="https://image.shutterstock.com/image-photo/javascript-text-toddler-girl-using-600w-1011796354.jpg"
-      alt="Third slide"
-    />
+          <Carousel.Caption>
+            <h3>20% Season OFF</h3>
+            <p>Always sold at a discounted price.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="https://image.shutterstock.com/image-photo/javascript-text-toddler-girl-using-600w-1011796354.jpg"
+            alt="Third slide"
+          />
 
-    <Carousel.Caption>
-      <h3>20% Season OFF</h3>
-      <p>Always sold at a discounted price.</p>
-    </Carousel.Caption>
-  </Carousel.Item>
-</Carousel>
-    <div className='container'>
-      <div className='row'>
-        <div className='col-md-4'>
-          <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%"
-          alt ="error occured"
-          />
-          <h4>{ shoes[0].title }</h4>
-          <p>{shoes[0].content}&{shoes[0].price}</p>
-        </div>
-        <div className='col-md-4'>
-        <img src="https://codingapple1.github.io/shop/shoes2.jpg" width="100%"
-          alt ="error occured"
-          />
-          <alt src="displayerror"/>
-          <h4>{ shoes[1].title }</h4>
-          <p>{shoes[1].content}</p>
-        </div>
-        <div className='col-md-4'>
-          <img src="https://codingapple1.github.io/shop/shoes3.jpg" width="100%"
-          alt ="error occured"
-          />
-          <h4>contents</h4>
-          <p>description&price</p>
+          <Carousel.Caption>
+            <h3>20% Season OFF</h3>
+            <p>Always sold at a discounted price.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-4">
+            <img
+              src="https://codingapple1.github.io/shop/shoes1.jpg"
+              width="100%"
+              alt="error occured"
+            />
+            <h4>{shoes[0].title}</h4>
+            <p>{shoes[0].content}</p>
+            <p>{shoes[0].price}</p>
+          </div>
+          <div className="col-md-4">
+            <img
+              src="https://codingapple1.github.io/shop/shoes2.jpg"
+              width="100%"
+              alt="error occured"
+            />
+            <alt src="displayerror" />
+            <h4>{shoes[1].title}</h4>
+            <p>{shoes[1].content}</p>
+            <p>{shoes[1].price}</p>
+          </div>
+          <div className="col-md-4">
+            <img
+              src="https://codingapple1.github.io/shop/shoes3.jpg"
+              width="100%"
+              alt="error occured"
+            />
+            <h4>{shoes[2].title}</h4>
+            <p>{shoes[2].content}</p>
+            <p>{shoes[2].price}</p>
+          </div>
         </div>
       </div>
-    </div>
     </div>
   );
 }
