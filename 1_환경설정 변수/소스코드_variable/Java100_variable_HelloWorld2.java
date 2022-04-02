d6f467ec59012c4f7df887ca61b270c23a46759e
OceansProgram <qkrekgka2003@gmail.com>
2022-04-02 20:28:53 +0900

    TIL 4.02 Java_Problem Sol

---
+++ 1_환경설정 변수/소스코드_variable/Java100_variable_HelloWorld2.java
@@ -0,0 +1,21 @@
+public class Java100_variable_HelloWorld2 {
+	public static void main(String[] args) {
+		System.out.println( "Hello World~" );
+	}
+}
+
+접근제한자    클래스선언    클래스이름 {
+	접근제한자 static 반환타입 메서드이름( 파라미터 ) {
+		// 구현할 코드 작성
+	}
+}
+
+(1) 접근제한자 --> public, private, protected, default
+				--> 클래스나 메서드에 접근할 수 있는 범위를 지정 --> 접근제한자, 접근제어자
+				--> private > protected > public
+(2) 클래스선언 --> class 
+				--> 객체를 생성하는 틀, 프레임, 공장, 템플릿
+(3) 클래스이름 --> 카멜케이스(단어와 단어 사이의 구분 --> 대문자)
+(4) 메서드이름 --> 메서드란? --> 함수 --> 어떤 특정한 동작이나 작업, 행위 등을 수행하는 것.
+
+
---
+++ Java/JavaSol100/100problem/src/Problem_1.java
@@ -0,0 +1,22 @@
+
+public class Problem_1 {
+	public static void main(String[] args) {
+		System.out.println("temp");
+	}
+
+}
+/* 
+ 접근제한자 클래스선언 클래스이름 {
+	접근제한자 static 반환타입 메서드이름(파라미터) {
+		//구현할 코드 작성
+	}
+
+}
+(1) 접근제한자 --> public,private,protected,default
+			--> 클래스나 메서드에 접근할 수 있는 범위를 지정 --> 접근제한자,접근제어자
+			--> private>protected>public
+(2) 클래스선언 --> class 
+			--> 
+(3) 클래스이름 --> 카멜케이스(단어와 단어사이의 구분 --> 대문자)
+ */
+ 
