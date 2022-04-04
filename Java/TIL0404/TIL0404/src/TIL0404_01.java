b122986391837a75025033c8c54f6b33c237011f
Oceansprogram <qkrekgka2003@gmail.com>
2022-04-04 17:24:04 +0900

    TIL 04.04 java Rewind(9)

--- Java/TIL0404/TIL0404/src/TIL0404_01.java
+++ Java/TIL0404/TIL0404/src/TIL0404_01.java
@@ -2,6 +2,17 @@ import java.util.*;
 
 public class TIL0404_01 {
 	public static void main(String[] args) {
-		System.out.print("");
+		Television tv = new Television();
+		tv.channel = 7;
+		tv.volume = 9;
+		tv.onOff = true;
+		int ch = tv.getChannel();
+		System.out.println("현재 체널은 " + ch +"입니다.");
+/*		tv.print();
+		Television yourTv = new Television();
+		yourTv.channel = 9;
+		yourTv.volume=12;
+		yourTv.onOff= true;
+		yourTv.print(); */
 	}
 }
---
+++ Java/TIL0404/TIL0404/src/Television.java
@@ -0,0 +1,13 @@
+
+public class Television {
+	int channel;
+	int volume;
+	boolean onOff;
+	void print() {
+		System.out.println("체널은 "+channel + "이고 볼륨은 "+volume + "입니다." );
+	}
+	int getChannel() {
+		return channel;
+	}
+	
+}
