0fcc12b170e346822d42c0688913c13186d76227
OceansProgram <qkrekgka2003@gmail.com>
2022-04-07 20:57:52 +0900

    TIL 4.07 Java_Problem Sol_4

--- Java/JavaSol100/100problem/src/Problem_4.java
+++ Java/JavaSol100/100problem/src/Problem_4.java
@@ -2,6 +2,18 @@
 public class Problem_4 {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		int a; // --정수가 저장될 변수 이름을 a로 만들어라--;;
+		int b;
+		int sum;
+		
+		a=3;
+		b=5;
+		sum = a+b;
+		System.out.println(sum);
+		
+		sum = a+b+a;
+		System.out.println(sum);
+		
 	}
 
 }
@@ -11,7 +23,7 @@ public class Problem_4 {
 	2. 변수선언이랑 무엇인가?
 	3. 변수의 용도는 무엇이고 왜 필요한가?
 	
-	[1]:변수란 무엇인가? --> 바구니다. --> 데이터를 저장하는 메모리 공간인다.
+	[1]:변수란 무엇인가? --> 바구니다. --> 데이터를 저장하는 메모리 공간인다. -->변하는 수.
 	[2]:변수 선언이란 무엇인가? -->변수를 사용하기 위해서는 먼저 변수의 타입에 맞는 선언을 해 줘야 한다.
 	   // 정수형의 변수를 사용하고자 한다면 --> 먼저 정수형 타입의 자료형을 선언해주고 사용해야 한다. -->
 	      데이터 타입(자료형)에 대해서는 뒤에...
