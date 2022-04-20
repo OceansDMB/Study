581a4fb3463fb55f3383feadc80aba2a42b54ecc
Oceansprogram <qkrekgka2003@gmail.com>
2022-04-20 14:44:51 +0900

    TIL 04.20 Python Rewind(1)

---
+++ python/workspace/circle.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 20 14:09:22 2022
+
+@author: user
+"""
+print('원의 반지름',4.0);
+print('원의 면적',3.14*4.0*4.0);
+print('원의 둘레',2.0*3.14*4.0);
---
+++ python/workspace/untitled0.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 20 13:48:32 2022
+
+@author: user
+"""
+print('당신의 이름은:');
+name = '홍길동'
+print(name);
