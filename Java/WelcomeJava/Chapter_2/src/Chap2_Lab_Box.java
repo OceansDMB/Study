eb838acaf4b46e64dfd3986cb64dbf4fa3e49111
OceansProgram <qkrekgka2003@gmail.com>
2022-05-08 04:12:56 +0900

    TIL 05.08 java Rewind(Welcome Java 2_fin)

---
+++ Java/WelcomeJava/Chapter_2/src/Chap2_Lab_Box.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Chap2_Lab_Box {
+	public static void main(String[] args) {
+		double w;
+		double h;
+		double area;
+		double perimeter;
+		
+		Scanner input = new Scanner(System.in);
+		
+	}
+}
