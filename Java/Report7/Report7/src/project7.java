39f2c7417a988a3e7c61f1bd31aef93f0bd6f12b
OceansProgram <qkrekgka2003@gmail.com>
2022-05-15 23:35:09 +0900

    TIL 05.15 java Rewind()

---
+++ Java/Report7/Report7/src/project7.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class project7 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int number;
+		while(number !=4) {
+			System.out.println("1.원 2.사각형 3.보기 4.종료 : ");				
+				number = sc.nextInt();
+				if(number == 1) {
+					
+			}
+			
+			System.out.print("x=");
+			int x = sc.nextInt();
+			System.out.print("y=");
+			int y = sc.nextInt();
+			System.out.print("r=");
+			int r = sc.nextInt();
+		}
+		
+	}
+}
