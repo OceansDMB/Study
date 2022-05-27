b54c3e4a09a35c7489ab9c24a9b4438c5d80a76b
Oceansprogram <qkrekgka2003@gmail.com>
2022-05-27 13:45:15 +0900

    TIL 5.27 Java_Problem Sol_36

--- 1.Java/Java/JavaSol100/100problem/src/Problem_36.java
+++ 1.Java/Java/JavaSol100/100problem/src/Problem_36.java
@@ -3,5 +3,18 @@
 
 
 public class Problem_36 {
-
+	public static void plusMethod(int a,int b) {
+		// 단순 출력
+		System.out.printf("인자로 넘겨받은 2개의 값은 %d와 %d입니다.%n",a,b);
+		int rst = a+b;
+		System.out.println("두 수를 더한 값은 "+rst);
+	}
+	
+	public static void main(String[] args) {
+		 
+		// [1] : 반환값 --> X		받는 인자값 --> O
+		// 메서드가 받는 인자값이 있다는 것은 호출부에서 파라미터 값을 넘긴다는 뜻.
+	int a = 100, b = 200;
+	plusMethod(a,b);
+	}
 }
