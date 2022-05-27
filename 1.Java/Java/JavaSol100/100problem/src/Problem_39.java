8cde1f4b45029d95cc574ad5750a4b3c331d532f
Oceansprogram <qkrekgka2003@gmail.com>
2022-05-27 17:54:38 +0900

    TIL 5.27 Java_Problem Sol_39

---
+++ 1.Java/Java/JavaSol100/100problem/src/Problem_39.java
@@ -0,0 +1,19 @@
+// static 선언이 안되어있는 메서드를 사용하는 방법에 대해서 코드로 구현해보시오. 
+// 이 문제는 메인 메서드에서 static 선언이 없는 메서드를 사용하는 방법에 대해서 하는지를 묻는 문제이다.
+// 클래스와 객체에 대한 선행 학습이 필요하므로 이에 대한 학습 후 문제를 다시 풀어보는것이 좋다.
+
+
+public class Problem_39 {
+	
+	public void helloWorld() {
+		System.out.println("Hello World");
+	}
+	
+	public static void main(String[] args){		
+		
+		
+		// [1] : 메서드 호출
+		// helloWorld()  //---  메인 메서드는 static 메서드만 호출할 수 있기 때문에 에러---
+	}
+	}
+
