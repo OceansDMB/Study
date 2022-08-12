90834b3d9c056fc7bab805769478090cb6975372
Oceansprogram <qkrekgka2003@gmail.com>
2022-08-12 17:08:29 +0900

    Python DataCrawling_(Selenium Version 4 update Program)_22.08.12_

--- 2.Python/WebCrawler/2_seleniuminfscroll.py
+++ 2.Python/WebCrawler/2_seleniuminfscroll.py
@@ -1,18 +1,27 @@
+from lib2to3.pgen2 import driver
+from tkinter import BROWSE
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from webdriver_manager.chrome import ChromeDriverManager
 import time
 # create browser
-browser = webdriver.Chrome('C:/chromedriver.exe')
+from webdriver_manager.chrome import ChromeDriverManager
+
 chrome_options = Options()
 chrome_options.add_experimental_option("detach", True)
+
+chrome_options.add_experimental_option("excludeSwitches", ["enable-logging"])
+
+service = Service(executable_path=ChromeDriverManager().install())
+browser = webdriver.Chrome(service=service, options=chrome_options)
 
 
 # open browser
 browser.get("https://www.naver.com")
-browser.implicitly_wait(10)
+browser.implicitly_wait(1)
 
 # Click Shopping menu
 browser.find_element(By.CSS_SELECTOR, 'a.nav.shop').click()
@@ -23,22 +32,32 @@ search = browser.find_element(
 search.click()
 
 # search val
-search.send_keys("포켓")
+search.send_keys("samsung memory")
 search.send_keys(Keys.ENTER)
 
 # ex_scroll_height
-beforeH = browser.execute_script("return window.scroolY")
+beforeH = browser.execute_script("return window.scrollY")
+
 
 while True:
     # 맨 아래로 스크롤을 내린다.
-    browser.find_elements(By.CSS_SELECTOR, 'body').send_keys(Keys.END)
+    browser.find_element(By.CSS_SELECTOR, "body").send_keys(Keys.END)
 
     # 스크롤 사이 페이지 로딩 시간
-    time.sleep(2)
+    time.sleep(0.1)
 
     # 스크롤 후 높이
-    afterH = browser.execute_script("return window.scroolY")
+    afterH = browser.execute_script("return window.scrollY")
 
     if afterH == beforeH:
         break
     beforeH = afterH
+# commercial div
+items = browser.find_elements(By.CSS_SELECTOR, "basicList_info_area__17Xyo")
+
+for item in items:
+    name = item.find_element(By.CSS_SELECTOR, "basicList_title__3P9Q7").text
+    price = item.find_element(By.CSS_SELECTOR, "price_num__2WUXn").text
+    link = item.find_element(
+        By.CSS_SELECTOR, "basicList_title__3P9Q7").get_attribute('href')
+    print(name, price, link)
