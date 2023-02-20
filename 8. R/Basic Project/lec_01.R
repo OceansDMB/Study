c53ee7638b37f18e615c85dea148cc9ab511b992
Oceansprogram <qkrekgka2003@gmail.com>
2023-02-20 17:24:31 +0900

    정보보안기사_필기 요약 정리 Day 1

---
+++ 8. R/Basic Project/lec_01.R
@@ -0,0 +1,50 @@
+a <-1
+a
+
+var1 <- c(1,2,5,7,8)
+var1
+
+var2 <- c(1:5)
+var2
+
+var3 <- seq(1,5)
+var3
+
+var4 <- seq(1,10,by=2)
+var4
+
+var5 <- seq(1,10,by=3)
+var5
+
+# 숫자 변수 연산 및 함수 사용
+
+var1
+var1+2
+
+var4
+max(var4)
+min(var4)
+sum(var4)
+mean(var4)
+median(var4)
+
+
+str1 <- "a"
+str1
+str2 <- "text"
+str2
+str3 <- "Hello World"
+str3
+str4 <- c("a","b","c")
+str4
+str5 <- c("Hello","World","is","good")
+str5
+
+paste(str5,collapse=",")
+paste(str5,collapse=" ")
+
+str6_1 <- 2022
+str6_2 <- 10
+str6_3 <- 20
+
+paste(str6_1,str6_2,str6_3,sep = "/")
